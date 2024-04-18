42c5014006f8e2ebbb6eda912fefa4a529d5e12e
ThisAbhinav <this.abhinav26@gmail.com>
2024-04-18 18:09:24 +0530

    as;dkja

--- dataloader.py
+++ dataloader.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from tqdm import tqdm
 import numpy as np
+import torch
 BASE_PATH = "../hms-harmful-brain-activity-classification"
 
 class CFG:
@@ -9,7 +10,12 @@ class CFG:
     label2name = dict(enumerate(class_names))
     name2label = {v:k for k, v in label2name.items()}
     
-sampling_rate = 200
+sampling_rate =  200
+
+def softmax(x):
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum()
+
 def load_data(no_of_eeg_id = 100,no_of_subid = 4):
     data = pd.read_csv(f'{BASE_PATH}/train.csv')
     df = data.copy()
@@ -25,25 +31,31 @@ def load_data(no_of_eeg_id = 100,no_of_subid = 4):
     df_counts = df.groupby('eeg_id')["eeg_id"].count()
     df_filtered = df[df['eeg_id'].isin(df_counts[df_counts >= no_of_subid].index)]
     list_of_eegs = df_filtered.groupby('eeg_id')["eeg_id"].count().index.tolist()
-
     x =[]
     y = []
     eeg_arr = {}
-    print("Loading the data!!!")
+    class_votes_names = ["gpd_vote","grda_vote","lpd_vote","lrda_vote","other_vote","seizure_vote"]
+    df_filtered["softmax"] = df_filtered[class_votes_names].apply(lambda x: softmax(x.tolist()),axis=1) 
     for eeg_id in tqdm(list_of_eegs[0:no_of_eeg_id]):
         df_multiple = df_filtered[df_filtered['eeg_id']==eeg_id]
         parquet_path = f'{BASE_PATH}/train_eegs/{eeg_id}.parquet'
         eeg_data = pd.read_parquet(parquet_path)
         eeg_arr[eeg_id] = eeg_data
-        for i in range(4):
+        for i in range(0,no_of_subid):
             eeg_label_offset_seconds=df_multiple[df_multiple["eeg_sub_id"]==i]["eeg_label_offset_seconds"].values[0]
             start_ind_sub_data = int(eeg_label_offset_seconds*sampling_rate)
             end_ind_sub_data = int((eeg_label_offset_seconds + 50) * sampling_rate)
             eeg_sub_data = eeg_data[start_ind_sub_data:end_ind_sub_data]["Fp1"].values
             x.append(eeg_sub_data)
-            y.append(df_multiple[df_multiple["eeg_sub_id"]==i]["class_label"].values[0])
+            y.append(df_multiple[df_multiple["eeg_sub_id"]==i]["softmax"].values[0])
+            if np.count_nonzero(np.isnan(eeg_sub_data)) > 0:
+                inv_nan = ~np.isnan(eeg_sub_data)
+                xp = inv_nan.nonzero()[0] # getting the indices of non-nan values
+                fp = eeg_sub_data[inv_nan] # getting the values of non-nan values
+                n  = np.isnan(eeg_sub_data).nonzero()[0] # getting the indices of nan values
+                eeg_sub_data[np.isnan(eeg_sub_data)] = np.interp(n, xp, fp) # linear interpolation
 
     X = np.array(x)
     Y = np.array(y)
-
     return X,Y
+
--- train.py
+++ train.py
@@ -6,48 +6,53 @@ import torch.nn as nn
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import dataloader
-device  = 'cuda' if torch.cuda.is_available() else 'cpu'
+# device  = 'cuda' if torch.cuda.is_available() else 'cpu'
+device = 'cpu'
 
 def accuracy_fn(y_true, y_pred):
     correct = torch.eq(y_true, y_pred).sum().item() # torch.eq() calculates where two tensors are equal
     acc = (correct / len(y_pred)) * 100 
     return acc
 
-n_epochs = 1
+n_epochs = 10
 lr = 0.001
 
 torch.manual_seed(0)
-X,Y = dataloader.load_data(100,4)
-x_train,x_test,y_train,y_test = train_test_split(X,Y,test_size=0.2)
-print(y_train.shape)
+X,Y = dataloader.load_data(10,6)
+x_train,x_test,y_train,y_test = train_test_split(X,Y,test_size=0.2,random_state=0)
 model = DenseNet(layer_num=(6,12,24,16),growth_rate=32,in_channels=1,classes=6) # model
 
 loss_fn = nn.KLDivLoss(reduction="batchmean") # loss function
 optimizer = torch.optim.Adam(model.parameters(), lr=lr) # optimizer
 model= model.to(device)
-x_train= torch.from_numpy(x_train).unsqueeze(1) # 320,1000 to 320,1,1000
-# y_train= torch.from_numpy(y_train).unsqueeze(1)
-x_test= torch.from_numpy(x_test).unsqueeze(1)
-# y_test= torch.from_numpy(y_test).unsqueeze(1)
+
+x_train = torch.from_numpy(x_train).unsqueeze(1).to(device) # 320,1000 to 320,1,1000
+y_train = torch.from_numpy(y_train).to(device)
+x_test = torch.from_numpy(x_test).unsqueeze(1).to(device)
+y_test = torch.from_numpy(y_test).to(device)
+
+
+
+
 #to do 
-# for epoch in range(n_epochs):
+for epoch in range(n_epochs):
     
-#     y_pred = model(x_train) #forward pass 
-    # loss = loss_fn(y_pred,y_train) # calculate loss
-    # acc = accuracy_fn(y_true=y_train,  y_pred=y_pred) # calculate accuracy
+    y_pred = model(x_train) #forward pass 
+    loss = loss_fn(y_pred,y_train) # calculate loss
+    acc = accuracy_fn(y_true=y_train,  y_pred=y_pred) # calculate accuracy
     
-    # loss.backward() # backward pass
+    loss.backward() # backward pass
     
-    # optimizer.step() # update weights
+    optimizer.step() # update weights
      
-    # model.eval() # set model to evaluation mode
+    model.eval() # set model to evaluation mode
     
-    # with torch.inference_mode(): 
-    #     test_pred = model(x_test) # forward pass
+    with torch.inference_mode(): 
+        test_pred = model(x_test) # forward pass
         
-    #     test_loss = loss_fn(test_pred,y_test) # calculate loss
-    #     test_acc = accuracy_fn(y_true=y_test,y_pred=test_pred) # calculate accuracy
+        test_loss = loss_fn(test_pred,y_test) # calculate loss
+        test_acc = accuracy_fn(y_true=y_test,y_pred=test_pred) # calculate accuracy
         
         
     # if epoch % 100 == 0:
-    #     print(f"Epoch: {epoch} | Loss: {loss:.5f}, Accuracy: {acc:.2f}% | Test loss: {test_loss:.5f}, Test acc: {test_acc:.2f}%")
+    print(f"Epoch: {epoch} | Loss: {loss:.5f}, Accuracy: {acc:.2f}% | Test loss: {test_loss:.5f}, Test acc: {test_acc:.2f}%")
