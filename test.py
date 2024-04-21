5d5d18533a1a9a759ba2ee62412ed2e5a1c249dc
ThisAbhinav <this.abhinav26@gmail.com>
2024-04-21 19:53:37 +0530

    something

--- test.py
+++ test.py
@@ -5,13 +5,19 @@ import torch
 import torch.nn as nn
 import dataloader
 import time
+from torchsummary import summary
+import torchinfo
+
+device  = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 model = DenseNet(layer_num=(6,12,24,16),growth_rate=32,in_channels=1,classes=6)
+torchinfo.summary(model)
+lr = 0.001
 
 loss_fn = nn.KLDivLoss(reduction="batchmean") # loss function
 optimizer = torch.optim.Adam(model.parameters(), lr=lr) # optimizer
 model= model.to(device)
-PATH = "./saved_models/model_15.pth"
+PATH = "./saved_models/model_45.pth"
 checkpoint = torch.load(PATH)
 
 model.load_state_dict(checkpoint['model_state_dict'])
@@ -22,9 +28,10 @@ loss = checkpoint['loss']
 model.eval()
 
 # write data code 
-
-
+X,Y = dataloader.load_data(1,3)
+X = torch.from_numpy(X).unsqueeze(1).to(device)
+Y = torch.from_numpy(Y).to(device)
 with torch.inference_mode():
-    test_pred = model(x_test)
-    test_loss = loss_fn(test_pred,y_test)
+    test_pred = model(X)
+    test_loss = loss_fn(test_pred,Y)
     print(f"KL div loss: {test_loss:.5f}")
